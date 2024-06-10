526d3e6f604a3daf20db0311ef40b38eef7ff4d4
Vinhdat835 <vinhdat998@gmail.com>
2024-06-10 11:34:54 +0700

    11:34 10/6

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -54,7 +54,7 @@ const Banner = () => {
                             className={`mySlides fade ${index === slideIndex ? 'active' : ''}`}
                             key={item.id || index} // Use a unique key
                         >
-                            <img src={`${item.thumb_url}`} style={{ width: '100%', height: '100%' }} />
+                            <img src={`${item.thumb_url}`} style={{ width: '100%', height: '100%' }} alt={`${item.thumb_url}`} />
                             <div className="text">
                                 <h1>welcome .</h1>
                                 <p>Millions of movies, TV shows and people to discover. Explore now.</p>
@@ -77,72 +77,3 @@ const Banner = () => {
 
 export default Banner;
 
-
-
-
-/* eslint-disable jsx-a11y/alt-text */
-// import React, { useEffect, useState } from 'react';
-// import './banner.css';
-// import { getPhimCapNhat } from '../../Api/api';
-// import { Link } from 'react-router-dom';
-
-// const Banner = () => {
-//     const [phimCapNhat, setPhimCapNhat] = useState([]);
-//     const [slideIndex, setSlideIndex] = useState(0);
-//     const [keyword, setKeyword] = useState('');
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         // Sử dụng giá trị từ input để cập nhật state keyword
-//         setKeyword(e.target.elements.searchInput.value);
-//     }
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const { phimCapNhat } = await getPhimCapNhat();
-//                 setPhimCapNhat(phimCapNhat);
-//             } catch (error) {
-//                 console.error('Error fetching movies:', error);
-//             }
-//         };
-//         fetchData();
-//     }, []);
-
-//     useEffect(() => {
-//         const interval = setInterval(() => {
-//             setSlideIndex(prevIndex => (prevIndex + 1) % phimCapNhat.length);
-//         }, 60000);
-
-//         return () => clearInterval(interval);
-//     }, [phimCapNhat]);
-
-//     return (
-//         <div className="slideshow-container">
-//             {phimCapNhat ? (
-//                 phimCapNhat.map((item, index) => (
-//                     <div
-//                         className={`mySlides fade ${index === slideIndex ? 'active' : ''}`}
-//                         key={item.id || index}
-//                     >
-//                         <img src={`${item.thumb_url}`} alt={`Slide ${index + 1}`} style={{ width: '100%', height: '100%' }} />
-//                         <div className="text">
-//                             <h1>welcome .</h1>
-//                             <p>Millions of movies, TV shows and people to discover. Explore now.</p>
-//                         </div>
-//                         <form onSubmit={handleSubmit}>
-//                             <div className='search'>
-//                                 <input value={keyword} placeholder='Search for a movie or TV show....' />
-//                                 <a href={`/movie/search?keyword=${keyword}`}><button type='submit'>Search</button></a>
-//                             </div>
-//                         </form>
-//                     </div>
-//                 ))
-//             ) : (
-//                 <></>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default Banner;
